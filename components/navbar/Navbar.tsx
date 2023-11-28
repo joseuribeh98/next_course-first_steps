d93bbca22d12ae9a27fcc349fd7e7cc301d1b048
Jose Uribe <joseuribeh@gmail.com>
2023-11-27 20:50:01 -0500

    Added Link component to navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,11 +1,26 @@
+import { HomeIcon } from '@primer/octicons-react'
+import Link from 'next/link'
+
+const navItems = [
+    {path: '/contact', label: 'Contact'},
+    {path: '/about', label: 'About'},
+    {path: '/pricing', label: 'Pricing'},
+]
+
 export const Navbar = () => {
     return (
         <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-            <span> Home </span>
+            <Link href='/' className='flex items-center'>
+                <HomeIcon className='mr-2' />
+                <span> Home </span>
+            </Link>
             <div className="flex flex-1"></div>
-            <a className="mr-2" href="/contact"> Contact </a>
-            <a className="mr-2" href="/about"> About </a>
-            <a className="mr-2" href="/pricing"> Pricing </a>
+
+            {navItems.map(({path, label}) => (
+                <Link key={path} href={path} className='mr-2'>
+                    {label}
+                </Link>
+            ))}
         </nav>
     )
 }
